b360ada27ba4a76bed30d5fc2f648e70c450e745
Fabio Simeoni <fabio.simeoni@me.com>
2019-08-17 18:13:13 +0200

    restored normal index

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,12 @@ import { App, Scaffold } from "./lib/components";
 import { state } from "./state";
 import { scaffold } from "./components/scaffold"
 import { mockery } from "./mockery"
-import { Button } from "antd";
 
 
 
-// render(<App initState={state} mockery={mockery}>
-//     <Scaffold model={scaffold}/>
-//   </App>
-// , document.getElementById("root"));
-
-
-render(<Button>sdsadsa</Button>
+render(<App initState={state} mockery={mockery}>
+    <Scaffold model={scaffold}/>
+  </App>
 , document.getElementById("root"));
+
+
